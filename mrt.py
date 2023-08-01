81e8b2e32af158d1a88abe8354d7b831af688006
Lucas Neukirchner <lneukirchner123@gmail.com>
2023-08-01 01:21:45 -0400

    added pickling for number dictionary
    it now saves the dictionary of each number and its multiplicative persistence

--- mrt.py
+++ mrt.py
@@ -2,16 +2,31 @@ import pickle
 
 def main():
     try:
-        current_record = pickle.
+        mp_num_dict = pickle.load(open('mp_num_dict.pickle', 'rb'))      
+    except:
+        mp_num_dict = {}
+
+    print(mp_num_dict)
+
+    try:
+        current_record = mp_num_dict[max(mp_num_dict.keys())]
+    except:
+        current_record = 0
     
-    current_number = 0
-    current_record = 0
+    print(current_record)
+
+    current_number = current_record + 1
+    print(current_number)
+
     while(True):
-        multiplicative_resistance = test_number(current_number)
-        if multiplicative_resistance > current_record:
-            print(str(current_number) + " " + str(multiplicative_resistance))
-            current_list.write(str(current_number) + " " + str(multiplicative_resistance) + "\n")
-            current_record = multiplicative_resistance
+        multiplicative_persistence = test_number(current_number)
+        if multiplicative_persistence > current_record:
+            #print(str(current_number) + " " + str(multiplicative_persistence))
+            mp_num_dict[multiplicative_persistence] = current_number
+            print(mp_num_dict)
+            pickle.dump(mp_num_dict, open('mp_num_dict.pickle', 'wb'))
+            current_record = multiplicative_persistence
+
         current_number += 1
 
 def test_number(number):
